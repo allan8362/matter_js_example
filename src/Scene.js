3d551be25e045ac1c8a2776df4b49a7e53d59d3e
Allan Brown <allanbrown8362@gmail.com>
2019-10-19 23:45:07 +0100

    Getting better all the time

--- src/Scene.js
+++ src/Scene.js
@@ -29,18 +29,40 @@ class Scene extends Component {
         wireframes: false
       }
     });
+    console.log("render:", render);
 
-    var ballA = Bodies.circle(210, 100, 30, { restitution: 0.5 });
-    var ballB = Bodies.circle(110, 50, 30, { restitution: 0.5 });
+    var wallOptions = { isStatic: true };
+
     World.add(engine.world, [
       // walls - x, y, width, height, {options}
-      Bodies.rectangle(0, 0, 900, 50, { isStatic: true }),
-      Bodies.rectangle(0, 0, 50, 1200, { isStatic: true }),
-      Bodies.rectangle(0, 600, 900, 50, { isStatic: true }),
-      Bodies.rectangle(450, 0, 50, 1200, { isStatic: true })
-    ]); 
+      // Top
+      Bodies.rectangle(0, 0, 900, 50, wallOptions),
+      // Left
+      Bodies.rectangle(0, 0, 50, 1200, wallOptions),
+      // Bottom
+      Bodies.rectangle(0, 600, 900, 50, wallOptions),
+      // Right
+      Bodies.rectangle(450, 0, 50, 1200, wallOptions),
+      // Left Ledge
+      Bodies.rectangle(50, 450, 100, 20, wallOptions),
+      // Right Ledge
+      Bodies.rectangle(300, 450, 100, 20, wallOptions),
+      // Ball Release wall
+      Bodies.rectangle(345, 575, 20, 750, wallOptions),
+      // Top right angled wall
+      Bodies.rectangle(345, 30, 5, 250, {angle: -45, isStatic: true}),
+      // Bumper
+      Bodies.circle(140, 140, 50, {isStatic: true, render: {fillStyle: "#B22222"}})
+    ]);
+    var triangleWall = Bodies.rectangle(345, 30, 5, 5, wallOptions);
+    console.log(triangleWall);
 
-    World.add(engine.world, [ballA, ballB]);
+    var ballOptions = {restitution: 0.5, render: {fillStyle: "#C0C0C0"}}
+    // circles - x, y, radius, {options}
+    var pinball = Bodies.circle(390, 540, 20, ballOptions);
+    // var ballA = Bodies.circle(200, 20, 30, ballOptions);
+    // var ballB = Bodies.circle(110, 50, 30, ballOptions);
+    // World.add(engine.world, [ballA, ballB]);
 
     // add mouse control
     var mouse = Mouse.create(render.canvas),
@@ -57,11 +79,27 @@ class Scene extends Component {
     World.add(engine.world, mouseConstraint);
 
     Matter.Events.on(mouseConstraint, "mousedown", function(event) {
-      World.add(engine.world, Bodies.circle(150, 50, 30, { restitution: 0.7 }));
+      console.log("x: ", event.mouse.position.x);
+      console.log("y: ", event.mouse.position.y);
+      // World.add(engine.world, Bodies.circle(150, 50, 20, ballOptions));
     });
 
-    Engine.run(engine);
+    const launchPinball = function (event){
+      if(event.keyCode==32){
+        World.add(engine.world, [pinball]);
+        Matter.Body.setVelocity(pinball, { x: 0, y: -30});
+		    Matter.Body.setAngularVelocity(pinball, 0);
+        console.log("Prepare to launch!!!");
+      };
+    };
 
+    document.addEventListener('DOMContentLoaded', () => {
+    document.addEventListener('keydown', launchPinball, false);
+    });
+
+
+
+    Engine.run(engine);
     Render.run(render);
   }
 
